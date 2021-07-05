564d4c63084780125290fb0fd5fae0aca859429f
anaysoul <soulyana_lakew@outlook.com>
2021-07-05 19:59:49 -0400

    conditional content

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import './NewExpense.css';
+import NewExpense from './NewExpense';
 
 const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState('');
@@ -67,6 +68,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>Cancel</button>
         <button type="submit" >Add Expense</button>
       </div>
     </form>
